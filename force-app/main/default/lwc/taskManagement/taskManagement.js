823186ba4e33c3799125285e512856b1a41a5761
k2-mat <k.matsu9@gmail.com>
2023-01-02 23:30:20 +0900

    search tasks

--- force-app/main/default/lwc/taskManagement/taskManagement.js
+++ force-app/main/default/lwc/taskManagement/taskManagement.js
@@ -1,14 +1,54 @@
 import { LightningElement } from 'lwc';
+import { loadStyle } from "lightning/platformResourceLoader";
+import NoHeader from "@salesforce/resourceUrl/NoHeader";
 import getTaskList from "@salesforce/apex/TaskManagementController.getTaskList";
 
 export default class TaskManagement extends LightningElement {
   searchResultTask;
+  searchTitle = '';
+  searchBody = '';
 
   connectedCallback() {
-    getTaskList().then((result) => {
-      this.searchResultTask = result;
-    }).catch((error) => {
+    loadStyle(this, NoHeader).then((result) => {});
+    getTaskList()
+      .then((result) => {
+        this.searchResultTask = result;
+      }).catch((error) => {
+        console.log(error);
+      })
+  }
 
-    })
+  changeSearchTitle(evt) {
+    this.searchTitle = evt.target.value;
+
+    getTaskList({title: this.searchTitle, body: this.searchBody})
+      .then((result) => {
+        this.searchResultTask = result;
+      }).catch((error) => {
+        console.log(error);
+      })
+  }
+
+  changeSearchBody(evt) {
+    this.searchBody = evt.target.value;
+
+    getTaskList({title: this.searchTitle, body: this.searchBody})
+      .then((result) => {
+        this.searchResultTask = result;
+      }).catch((error) => {
+        console.log(error);
+      })
+  }
+
+  handleReset() {
+    this.searchTitle = '';
+    this.searchBody = '';
+
+    getTaskList()
+      .then((result) => {
+        this.searchResultTask = result;
+      }).catch((error) => {
+        console.log(error);
+      })
   }
 }
