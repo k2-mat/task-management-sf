c3371145b528f8ca009bd4bcdf298f332ee1421d
k2-mat <k.matsu9@gmail.com>
2023-01-03 11:46:21 +0900

    crud tasks

---
+++ force-app/main/default/lwc/modalTaskForm/modalTaskForm.js
@@ -0,0 +1,88 @@
+import { LightningElement, api } from 'lwc';
+import getTask from "@salesforce/apex/ModalTaskFormController.getTask";
+
+export default class ModalTaskForm extends LightningElement {
+  showModal = false;
+  task = {};
+  recordid;
+
+  @api show(recordid) {
+    this.showModal = true;
+    this.setData(recordid);
+  }
+
+  @api hide() {
+    this.task = {};
+    this.showModal = false;
+  }
+
+  get title() {
+    return this.task.Title__c;
+  }
+
+  get body() {
+    return this.task.Body__c;
+  }
+
+  get headerTitle() {
+    return this.recordid ? '編集' : '登録';
+  }
+
+  get submitTitle() {
+    return this.recordid ? '更新する' : '登録する';
+  }
+
+  setData(recordid) {
+    this.recordid = recordid;
+    if (this.recordid) {
+      getTask({recordid: this.recordid})
+        .then((result) => {
+          this.task = result;
+        }).catch((error) => {
+          console.log(errror);
+        })
+    }
+  }
+
+  handleChangeValue(event) {
+    this.task[event.target.name] = event.target.value;
+  }
+
+  handleDialogClose() {
+    this.dispatchEvent(
+        new CustomEvent("setvalue", {
+            detail: {
+                value: null
+            }
+        })
+    );
+    this.hide();
+}
+
+  handleSave() {
+    if (this.checkValidForm()) {
+        this.dispatchEvent(
+            new CustomEvent("setvalue", {
+                detail: {
+                    value: this.task
+                }
+            })
+        );
+        this.hide();
+    }
+  }
+
+  checkValidForm() {
+    this.template.querySelectorAll(".input").forEach((input) => {
+        input.setCustomValidity("");
+    });
+    let allValid = [...this.template.querySelectorAll(".input")].reduce(
+        (validSoFar, inputCmp) => {
+            inputCmp.reportValidity();
+            return validSoFar && inputCmp.checkValidity();
+        },
+        true
+    );
+    return allValid;
+  }
+}
--- force-app/main/default/lwc/taskManagement/taskManagement.js
+++ force-app/main/default/lwc/taskManagement/taskManagement.js
@@ -1,21 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { loadStyle } from "lightning/platformResourceLoader";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import NoHeader from "@salesforce/resourceUrl/NoHeader";
 import getTaskList from "@salesforce/apex/TaskManagementController.getTaskList";
+import createTask from "@salesforce/apex/TaskManagementController.createTask";
+import updateTask from "@salesforce/apex/TaskManagementController.updateTask";
+import deleteTask from "@salesforce/apex/TaskManagementController.deleteTask";
+
+import LightningConfirm from 'lightning/confirm';
 
 export default class TaskManagement extends LightningElement {
-  searchResultTask;
   searchTitle = '';
   searchBody = '';
 
+  @track searchResultTask;
+
   connectedCallback() {
     loadStyle(this, NoHeader).then((result) => {});
-    getTaskList()
-      .then((result) => {
-        this.searchResultTask = result;
-      }).catch((error) => {
-        console.log(error);
-      })
+    this.reloadData();
   }
 
   changeSearchTitle(evt) {
@@ -25,7 +27,7 @@ export default class TaskManagement extends LightningElement {
       .then((result) => {
         this.searchResultTask = result;
       }).catch((error) => {
-        console.log(error);
+        showError('タスクを取得できませんでした。');
       })
   }
 
@@ -36,19 +38,88 @@ export default class TaskManagement extends LightningElement {
       .then((result) => {
         this.searchResultTask = result;
       }).catch((error) => {
-        console.log(error);
+        showError('タスクを取得できませんでした。');
       })
   }
 
   handleReset() {
     this.searchTitle = '';
     this.searchBody = '';
+    this.reloadData();
+  }
 
+  openModalNew(evt) {
+    const modal = this.template.querySelector(
+      "c-modal-task-form"
+    );
+    modal.show(evt.target.dataset.recordid);
+  }
+
+  handleSetInputValue(evt) {
+    if (evt.detail.value) {
+      if (evt.detail.value.Id) {
+        updateTask({task: evt.detail.value})
+          .then((result) => {
+            this.showMessage('タスクを更新しました。');
+            this.reloadData();
+          }).catch((error) => {
+            this.showError('タスクを更新できませんでした。');
+          })
+      } else {
+        createTask({task: evt.detail.value})
+          .then((result) => {
+            this.showMessage('タスクを登録しました。');
+            this.reloadData();
+          }).catch((error) => {
+            this.showError('タスクを登録できませんでした。');
+          })
+      }
+    }
+  }
+
+  showMessage(message) {
+    this.dispatchEvent(
+      new ShowToastEvent({
+          title: message,
+          variant: "success"
+      })
+    );
+  }
+
+  showError(title, message) {
+    this.dispatchEvent(
+        new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: "error",
+            mode: "sticky"
+        })
+    );
+  }
+
+  reloadData() {
     getTaskList()
       .then((result) => {
         this.searchResultTask = result;
       }).catch((error) => {
-        console.log(error);
-      })
+        showError('タスクを取得できませんでした。');
+      });
+  }
+
+  async handleDeleteTask(evt) {
+    const recordid = evt.target.dataset.recordid;
+    const result = await LightningConfirm.open({
+      message: '本当に削除しますか？',
+      variant: 'headerless'
+    });
+    if (result) {
+      deleteTask({recordid: recordid})
+        .then((result) => {
+          this.showMessage('タスクを削除しました。');
+          this.reloadData();
+        }).catch((error) => {
+          showError('タスクを削除できませんでした。');
+        });
+    }
   }
 }
